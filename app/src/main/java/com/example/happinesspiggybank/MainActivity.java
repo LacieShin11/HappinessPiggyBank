d281acf6bed3bf393299aa5c256f73494f6fc311
Lacie Shin <lacieshin11@gmail.com>
2020-02-25 02:04:20 +0900

    [상점] 메인 액티비티에 상점 액티비티 연결

--- app/src/main/java/com/example/happinesspiggybank/MainActivity.java
+++ app/src/main/java/com/example/happinesspiggybank/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.happinesspiggybank;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button shopBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        shopBtn = (Button) findViewById(R.id.shop);
+
+        shopBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent shopIntent = new Intent(getApplicationContext(), ShopActivity.class);
+                startActivity(shopIntent);
+            }
+        });
     }
 }
