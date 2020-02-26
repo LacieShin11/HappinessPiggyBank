cf9ce2ad67a8865b1823531e54d16ced61f0fff3
kittymew <kitty!2101?>
2020-02-27 03:38:14 +0900

    [추가]상단바 삭제, 추가 액티비티 생성 및 날짜와 시간 받아오기

--- app/src/main/java/com/example/happinesspiggybank/MainActivity.java
+++ app/src/main/java/com/example/happinesspiggybank/MainActivity.java
@@ -5,23 +5,34 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    private Button shopBtn;
+    private Button shopBtn, creationBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
         shopBtn = (Button) findViewById(R.id.shop);
+        creationBtn = (Button) findViewById(R.id.creat_happiness);
 
         shopBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent shopIntent = new Intent(getApplicationContext(), ShopActivity.class);
                 startActivity(shopIntent);
+            }
+        });
+
+        creationBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent createIntent = new Intent(getApplicationContext(), CreateActivity.class);
+                startActivity(createIntent);
             }
         });
     }
